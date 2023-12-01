044ed772a262a4ddf9fcc9e8ae5dddd58309dd32
a_nightails <a.nightails@gmail.com>
2023-12-01 09:04:53 -0600

    Initialization of the weapon framework module

---
+++ Private/WeaponFramework.cpp
@@ -0,0 +1,17 @@
+﻿#include "WeaponFramework.h"
+
+#define LOCTEXT_NAMESPACE "FWeaponFrameworkModule"
+
+void FWeaponFrameworkModule::StartupModule()
+{
+    
+}
+
+void FWeaponFrameworkModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWeaponFrameworkModule, WeaponFramework)
