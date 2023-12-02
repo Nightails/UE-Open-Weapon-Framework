ec099fe55b2473d1599bae4fc376e44c5b3f0dbc
a_nightails <a.nightails@gmail.com>
2023-12-02 14:51:16 -0600

    Initializing component

--- Private/WF_FireArm.cpp
+++ Private/WF_FireArm.cpp
@@ -6,8 +6,12 @@
 
 AWF_FireArm::AWF_FireArm()
 {
+	// Initializing variables
 	FiringRate = 5.f;
 	SpreadMaxAngle = 5.f;
+
+	// Initializing components
+	FireArmSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon Mesh");
 	
 	// Disable tick at the moment, due to no usage.
 	PrimaryActorTick.bCanEverTick = false;
